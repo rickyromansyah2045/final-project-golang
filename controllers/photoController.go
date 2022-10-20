54d94e13d41e9284f3f2888c30acbc94e6bde4a1
RomansyahOfficial <ricky@hotelmurah.com>
2022-10-20 12:05:12 +0700

    Updated

--- controllers/photoController.go
+++ controllers/photoController.go
@@ -1,1 +1,231 @@
 package controllers
+
+import (
+	"final-project-golang/helpers"
+	"final-project-golang/models"
+	"net/http"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type PhotoController struct {
+	db *gorm.DB
+}
+
+type PhotoCreateRequest struct {
+	Title    string `json:"title"`
+	Caption  string `json:"caption"`
+	PhotoUrl string `json:"photo_url"`
+}
+
+type PhotoCreateResponse struct {
+	Id        uint       `json:"id"`
+	Title     string     `json:"title"`
+	Caption   string     `json:"caption"`
+	PhotoUrl  string     `json:"photo_url"`
+	UserId    uint       `json:"user_id"`
+	CreatedAt *time.Time `json:"created_at"`
+}
+
+type PhotoUpdateResponse struct {
+	Id        uint       `json:"id"`
+	Title     string     `json:"title"`
+	Caption   string     `json:"caption"`
+	PhotoUrl  string     `json:"photo_url"`
+	UserId    uint       `json:"user_id"`
+	UpdatedAt *time.Time `json:"updated_at"`
+}
+
+type PhotoGetResponse struct {
+	Id        uint       `json:"id"`
+	Title     string     `json:"title"`
+	Caption   string     `json:"caption"`
+	PhotoUrl  string     `json:"photo_url"`
+	UserId    uint       `json:"user_id"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	User      UserDataResponse
+}
+
+type UserDataResponse struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
+
+func NewPhotoController(db *gorm.DB) *PhotoController {
+	return &PhotoController{
+		db: db,
+	}
+}
+
+func (p *PhotoController) Create(ctx *gin.Context) {
+	userId, _ := ctx.Get("id")
+	var photoReq PhotoCreateRequest
+
+	err := ctx.ShouldBindJSON(&photoReq)
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	newPhoto := models.Photo{
+		Title:    photoReq.Title,
+		Caption:  photoReq.Caption,
+		PhotoUrl: photoReq.PhotoUrl,
+		UserId:   uint(userId.(float64)),
+	}
+
+	_, errCreate := govalidator.ValidateStruct(&newPhoto)
+	if errCreate != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	err = p.db.Create(&newPhoto).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, err.Error())
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	response := PhotoCreateResponse{
+		Id:        newPhoto.Id,
+		Title:     newPhoto.Title,
+		Caption:   newPhoto.Caption,
+		PhotoUrl:  newPhoto.PhotoUrl,
+		UserId:    newPhoto.UserId,
+		CreatedAt: newPhoto.CreatedAt,
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusCreated, response)
+}
+
+func (p *PhotoController) Get(ctx *gin.Context) {
+	var photos []models.Photo
+
+	err := p.db.Preload("User").Find(&photos).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, err.Error())
+			return
+		}
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	var response []PhotoGetResponse
+	for _, photo := range photos {
+		var userData UserDataResponse
+		if photo.User != nil {
+			userData = UserDataResponse{
+				Username: photo.User.Username,
+				Email:    photo.User.Email,
+			}
+		}
+		response = append(response, PhotoGetResponse{
+			Id:        photo.Id,
+			Title:     photo.Title,
+			Caption:   photo.Caption,
+			PhotoUrl:  photo.PhotoUrl,
+			UserId:    photo.UserId,
+			CreatedAt: photo.CreatedAt,
+			UpdatedAt: photo.UpdatedAt,
+			User:      userData,
+		})
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusOK, response)
+}
+
+func (p *PhotoController) Update(ctx *gin.Context) {
+	userId, _ := ctx.Get("id")
+	photoId := ctx.Param("photoId")
+	var photoReq PhotoCreateRequest
+	var photo models.Photo
+
+	err := ctx.ShouldBindJSON(&photoReq)
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	updatePhoto := models.Photo{
+		Title:    photoReq.Title,
+		Caption:  photoReq.Caption,
+		PhotoUrl: photoReq.PhotoUrl,
+	}
+
+	err = p.db.First(&photo, photoId).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, "data not found")
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	if photo.UserId != uint(userId.(float64)) {
+		helpers.UnauthorizeJsonResponse(ctx, "you're not allowed to update or edit this photo")
+		return
+	}
+
+	err = p.db.Model(&photo).Updates(updatePhoto).Error
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	response := PhotoUpdateResponse{
+		Id:        photo.Id,
+		Title:     photo.Title,
+		Caption:   photo.Caption,
+		PhotoUrl:  photo.PhotoUrl,
+		UserId:    photo.UserId,
+		UpdatedAt: photo.UpdatedAt,
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusOK, response)
+}
+
+func (p *PhotoController) Delete(ctx *gin.Context) {
+	userId, _ := ctx.Get("id")
+	photoId := ctx.Param("photoId")
+	var photo models.Photo
+
+	err := p.db.First(&photo, photoId).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, "data not found")
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	if photo.UserId != uint(userId.(float64)) {
+		helpers.UnauthorizeJsonResponse(ctx, "you're not allowed to delete this photo")
+		return
+	}
+
+	err = p.db.Delete(&photo).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, err.Error())
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusOK, gin.H{
+		"message": "Your photo has been successfully deleted",
+	})
+
+}
--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "gorm.io/gorm"
+import (
+	"final-project-golang/helpers"
+	"final-project-golang/models"
+	"net/http"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type UserController struct {
 	db *gorm.DB
@@ -21,4 +30,201 @@ type UserLoginRequest struct {
 type UserUpdateRequest struct {
 	Email    string `json:"email" valid:"email~Invalid format email"`
 	Username string `json:"username"`
+}
+
+type UserRegisterResponse struct {
+	Id       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Age      int    `json:"age"`
+}
+
+type UserUpdateResponse struct {
+	Id        uint       `json:"id"`
+	Username  string     `json:"username"`
+	Email     string     `json:"email"`
+	Age       int        `json:"age"`
+	UpdatedAt *time.Time `json:"updated_at"`
+}
+
+func NewUserController(db *gorm.DB) *UserController {
+	return &UserController{
+		db: db,
+	}
+}
+
+func (u *UserController) Register(ctx *gin.Context) {
+	var userReq UserRegisterRequest
+
+	err := ctx.ShouldBindJSON(&userReq)
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	newUser := models.User{
+		Age:      int(userReq.Age),
+		Email:    userReq.Email,
+		Username: userReq.Username,
+		Password: userReq.Password,
+	}
+
+	_, errCreate := govalidator.ValidateStruct(&newUser)
+	if errCreate != nil {
+		helpers.BadRequestResponse(ctx, errCreate.Error())
+		return
+	}
+
+	err = u.db.Create(&newUser).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, err.Error())
+			return
+		}
+		if err.Error() == `ERROR: duplicate key value violates unique constraint "idx_users_username" (SQLSTATE 23505)` {
+			helpers.BadRequestResponse(ctx, "username is duplicated")
+			return
+		}
+		if err.Error() == `ERROR: duplicate key value violates unique constraint "idx_users_email" (SQLSTATE 23505)` {
+			helpers.BadRequestResponse(ctx, "email is duplicated")
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	response := UserRegisterResponse{
+		Id:       newUser.Id,
+		Username: newUser.Username,
+		Email:    newUser.Email,
+		Age:      newUser.Age,
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusCreated, response)
+}
+
+func (u *UserController) Login(ctx *gin.Context) {
+	var userReq UserLoginRequest
+
+	err := ctx.ShouldBindJSON(&userReq)
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	loginUser := models.User{
+		Email:    userReq.Email,
+		Password: userReq.Password,
+	}
+
+	_, errCreate := govalidator.ValidateStruct(&userReq)
+	if errCreate != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	err = u.db.First(&loginUser, "email=?", userReq.Email).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, "email not found")
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	isValid := helpers.ComparePassword(loginUser.Password, userReq.Password)
+
+	if !isValid {
+		helpers.UnauthorizeJsonResponse(ctx, "username / password is not match")
+		return
+	}
+
+	token, err := helpers.GenerateToken(loginUser.Id, loginUser.Email)
+	if err != nil {
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusOK, gin.H{
+		"token": token,
+	})
+}
+
+func (u *UserController) Update(ctx *gin.Context) {
+	userId, _ := ctx.Get("id")
+	var userReq UserUpdateRequest
+	var user models.User
+
+	err := ctx.ShouldBindJSON(&userReq)
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	updateUser := models.User{
+		Email:    userReq.Email,
+		Username: userReq.Username,
+	}
+
+	_, errUpdate := govalidator.ValidateStruct(&userReq)
+	if errUpdate != nil {
+		helpers.BadRequestResponse(ctx, errUpdate.Error())
+		return
+	}
+
+	err = u.db.First(&user, userId).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, "User data not found")
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	err = u.db.Model(&user).Updates(updateUser).Error
+	if err != nil {
+		helpers.BadRequestResponse(ctx, err.Error())
+		return
+	}
+
+	response := UserUpdateResponse{
+		Id:        user.Id,
+		Username:  user.Username,
+		Email:     user.Email,
+		Age:       user.Age,
+		UpdatedAt: user.UpdatedAt,
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusOK, response)
+}
+
+func (u *UserController) Delete(ctx *gin.Context) {
+	userId, _ := ctx.Get("id")
+	var user models.User
+
+	err := u.db.First(&user, userId).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.BadRequestResponse(ctx, "User not found")
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	err = u.db.Delete(&user).Error
+	if err != nil {
+		if err.Error() == gorm.ErrRecordNotFound.Error() {
+			helpers.NotFoundResponse(ctx, err.Error())
+			return
+		}
+		helpers.InternalServerJsonResponse(ctx, err.Error())
+		return
+	}
+
+	helpers.WriteJsonResponse(ctx, http.StatusOK, gin.H{
+		"message": "Your account has been successfully deleted",
+	})
 }
